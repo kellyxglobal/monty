d04542c50f6bb64582978c68b7ba2031ed1449a1
Kellyxglobal-WebS <39860700+kellyxglobal@users.noreply.github.com>
2022-09-08 23:39:01 +0100

    commit main

--- main.c
+++ main.c
@@ -1,103 +1,24 @@
+#ifndef _LISTS
+#define _LISTS
+
 #include "monty.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-global_t vglo;
+typedef stack_t dlistint_t;
 
-/**
- * free_vglo - frees the global variables
- *
- * Return: no return
- */
-void free_vglo(void)
-{
-	free_dlistint(vglo.head);
-	free(vglo.buffer);
-	fclose(vglo.fd);
-}
+/* dlist1.c */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
-/**
- * start_vglo - initializes the global variables
- *
- * @fd: file descriptor
- * Return: no return
- */
-void start_vglo(FILE *fd)
-{
-	vglo.lifo = 1;
-	vglo.cont = 1;
-	vglo.arg = NULL;
-	vglo.head = NULL;
-	vglo.fd = fd;
-	vglo.buffer = NULL;
-}
+/* dlist2.c */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
 
-/**
- * check_input - checks if the file exists and if the file can
- * be opened
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: file struct
- */
-FILE *check_input(int argc, char *argv[])
-{
-	FILE *fd;
-
-	if (argc == 1 || argc > 2)
-	{
-		dprintf(2, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fd = fopen(argv[1], "r");
-
-	if (fd == NULL)
-	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	return (fd);
-}
-
-/**
- * main - Entry point
- *
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	void (*f)(stack_t * *stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
-
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
-	{
-		lines[0] = _strtok(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_opcodes(lines[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
-				exit(EXIT_FAILURE);
-			}
-			vglo.arg = _strtok(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
-		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
-	}
-
-	free_vglo();
-
-	return (0);
-}
+#endif
